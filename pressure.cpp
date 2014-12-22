ed4f60b4d71222a775b53d28a4c61ed2d6be1809
Sean Staz <seanstaz52@gmail.com>
2014-12-21 19:54:37 -0600

    Create pressure.cpp
    Sean Staz
    CSC 2134
    Program Pressure prints appropriate messages based on a pressure reading read from a file. Processing continues until the plant is evacuated because of a pressure reading over 100.

---
+++ pressure.cpp
@@ -0,0 +1,32 @@
+// Sean Staz
+// CSC 2134
+
+// Program Pressure prints appropriate messages based
+// on a pressure reading read from a file.  Processing
+// continues until the plant is evacuated because of
+// a pressure reading over 100. 
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+  int  pressure;
+  ifstream  data;
+  data.open("pressure.d1");
+                                          
+ while (pressure < 100)
+ {
+    data  >> pressure; 
+	
+	cout << "Pressure is okay." << endl; 
+ }
+ while (pressure >= 100)
+ {
+ cout << "Pressure has exceeded 100! Please evacute ASAP!" << endl;
+}
+    return 0;
+}                                                                               
